25d9d9a4498f7546ff02c1ddeb28f84450f63f56
marksg7 <gabriel.gmgabrielbmarks.marks@gmail.com>
2022-05-23 00:45:47 -0400

    g

--- txt-convert.py
+++ txt-convert.py
@@ -1,5 +1,17 @@
 import sys
 
+def stripEvil(s):
+    i = 0
+    fins = ''
+    while s.find('《', i) != -1:
+        st = s.find('《', i)
+        e = s.find('》', st+1)
+        assert e != -1 and 50 > e - st > 1, 'Gone wrong w evil stripping'
+        fins += s[i:st]
+        i = e + 1
+    fins += s[i:]
+    return fins
+
 assert len(sys.argv) >= 2
 
 fname = sys.argv[1]
@@ -22,6 +34,8 @@ with open(fname, 'r', encoding=ec) as f:
         html += '</p>'
 
 html += '</body>'
+
+html = stripEvil(html)
 
 with open('out.html', 'w', encoding='utf-8') as f:
     f.write(html)
